0072c8be1745466109f21f7ff3f873e230d9732e
Fábio Matavelli <fabio.matavelli@teto.com.br>
2018-10-25 21:56:43 -0300

    Update windows setup process

--- setup.ps1
+++ setup.ps1
@@ -7,10 +7,24 @@ New-Item -ItemType 'folder' -Path 'C:\' -Name 'wts'
 New-Item -ItemType 'folder' -Path 'C:\' -Name 'sys'
 New-Item -ItemType 'folder' -Path 'C:\' -Name 'sys\base'
 
-choco update chocolatey -y
+choco update chocolatey -y -V
 
-choco install googlechrome -y
-choco install rdmfree -y
-choco install github -y
-choco install notepadplusplus -y
-choco install firebird --version 2.5.7.1000 -params '/Classic' -y
+choco install googlechrome -y -V
+choco install slack -y -V
+
+choco install rdmfree -y -V
+choco install github -y -V
+choco install notepadplusplus -y -V
+choco install vscode -y -V
+
+choco install firebird --version 2.5.7.1000 -params '/Classic' -y -V
+
+choco install nodejs --version 8.12.0 -y -V
+
+choco install awscli -y -V
+choco install awstools.powershell -y -V
+
+(New-Object System.Net.WebClient).DownloadFile('https://dl.google.com/go/go1.11.1.windows-amd64.msi', "$env:HOMEPATH\Downloads\Go.msi")
+Start-Process msiexec.exe -Wait -ArgumentList "/I $env:HOMEPATH\Downloads\Go.msi /quiet"
+
+choco install gpg4win -y -V
