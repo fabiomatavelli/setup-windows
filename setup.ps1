da83417bf1dcf7299bdfa0503fe22b762091180b
Fábio Matavelli <fabiomatavelli@gmail.com>
2018-11-07 11:09:19 -0200

    Create a list of packages
    Create a list of packages and loop through it, instead of type every command.

--- setup.ps1
+++ setup.ps1
@@ -7,30 +7,33 @@ New-Item -ItemType 'directory' -Path $env:HOMEDRIVE -Name 'wts'
 New-Item -ItemType 'directory' -Path $env:HOMEDRIVE -Name 'sys'
 New-Item -ItemType 'directory' -Path $env:HOMEDRIVE -Name 'sys\base'
 
+$packages = @("googlechrome", 
+              "slack",
+              "lightshot",
+              "rdmfree",
+              "git",
+              "github",
+              "tortoisegit",
+              "notepadplusplus",
+              "vscode",
+              "skype",
+              "cmder",
+              "cyberduck",
+              "python2",
+              "awscli",
+              "awstools.powershell",
+              "gpg4win")
+
 choco update chocolatey -y -V
 
-choco install googlechrome -y -V
+foreach($pkg in $packages) {
+  Start-Process choco -ArgumentList @("install", $pkg, "-y", "-V") -Wait
+}
+
 & "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 
-choco install slack -y -V
-choco install lightshot -y -V
-choco install rdmfree -y -V
-choco install git -y -V
-choco install github -y -V
-choco install tortoisegit -y -V
-choco install notepadplusplus -y -V
-choco install vscode -y -V
-choco install skype -y -V
-
-choco install firebird --version 2.5.7.1000 -params '/Classic' -y -V
-
-choco install nodejs --version 8.12.0 -y -V
-
-choco install python2 -y -V
-choco install awscli -y -V
-choco install awstools.powershell -y -V
+Start-Process choco -ArgumentList @("install","firebird","--version 2.5.7.1000","-params '/Classic'","-y","-V") -Wait
+Start-Process choco -ArgumentList @("install","nodejs","--version 8.12.0","-y","-V") -Wait
 
 (New-Object System.Net.WebClient).DownloadFile('https://dl.google.com/go/go1.11.1.windows-amd64.msi', "$env:HOMEPATH\Downloads\Go.msi")
 & .\msiexec.exe /i "$env:HOMEDRIVE\$env:HOMEPATH\Downloads\Go.msi" /quiet
-
-choco install gpg4win -y -V
